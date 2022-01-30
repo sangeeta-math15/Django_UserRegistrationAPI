04821d894b6478296d0582a498f7e0656f293f51
sngtmth <sngtmth@gmail.com>
2022-01-30 20:16:58 +0530

    Added:user login and registration api using user_auth model

--- account/user/views.py
+++ account/user/views.py
@@ -1,5 +1,7 @@
-
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.core.exceptions import FieldDoesNotExist, ValidationError
+from django.db import IntegrityError
 from django.http import JsonResponse
 from .models import Register
 import json
@@ -7,57 +9,46 @@ import json
 
 def user_register(request):
     """
-    This method register details and return response wether registation is register or not
-    :param request:
-    :return:
+     This method register user by using inbuilt User Models.
+    Using create_user method, storing details into database.
+    Returns response whether user registered or not.
     """
     if request.method == 'POST':
         try:
             # print(data)
             data = json.loads(request.body)
-            user_data = {
-                'user_name': data.get('user_name'),
-                'first_name': data.get('first_name'),
-                'last_name': data.get('last_name'),
-                'password': data.get('password')
-            }
-
-            if Register.objects.filter(user_name = data.get('user_name')):
-                data = {'message': 'username already exists..'}
-                return JsonResponse(data)
-            else:
-                user = Register.objects.create(**user_data)
-                data = {'Message': 'New registration registered successfully..'}
-                user.save()
-                return JsonResponse(data)
-
-        except FieldDoesNotExist:
-            exception = {'Exception': 'Field does not exists..'}
+            user = User.objects.create_user(username=data.get('username'),
+                                            password=data.get('password'),
+                                            first_name=data.get('first_name'),
+                                            last_name=data.get('last_name'))
+            data = {'message':'new user registered successfully..'}
+            return JsonResponse(data)
+        except IntegrityError:
+            exception = {'Exception':'already exists...!'}
             return JsonResponse(exception)
-
         except Exception as e:
-            exc = {'Exception': str(e)}
-            return JsonResponse(exc)
-
+            exc = {'Exception':str(e)}
 
 
 def user_login(request):
     """
-    This method logins registration based on username and password.
-    Returns login success or failed...
+   This method logins registration based on username and password by using authenticate method.
+    Returns response login is success or failed...
     """
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            if Register.objects.filter(user_name=data.get('user_name'), password=data.get('password')):
-                success_data = {'message': 'Login successful'}
-                return JsonResponse(success_data)
-            else:
-                fail_data = {'message': 'Invalid username or password...!'}
-                return JsonResponse(fail_data)
+            user = authenticate(username = data.get('username'),
+                                password = data.get('password'))
+            if user is not None:
+                data = {'message':'successfully logged in..!'}
+                return JsonResponse(data)
         except ValidationError:
-            exc_value = {'Exception': 'Values does not exists'}
+            exc_value = {'Exception':'Values does not exists'}
             return JsonResponse(exc_value)
-        except Exception as e:
-            value = {'Exception': str(e)}
-            return JsonResponse(value)
+
+
+
+
+
+
